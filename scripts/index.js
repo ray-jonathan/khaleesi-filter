d708b56307648ed7524a7c645bddfd2000de275b
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-24 16:31:29 -0400

    Remade most titles search using object instead of array, Added Valyrian count, Added Hot Pie actor search

--- scripts/index.js
+++ scripts/index.js
@@ -24,12 +24,30 @@ const numDead = deadCharacters.filter(died => died).length
 console.log(numDead);
 
 // Who has the most titles?
-const titleCharacters = characters.map(character => [character.name, character.titles.length]);
-console.log(titleCharacters);
-const sortedTitles = titleCharacters.sort(function (a,b) {return b[1]-a[1]})
-console.log(sortedTitles[0][0])
+// const titleCharacters = characters.map(character => [character.name, character.titles.length]);
+// const sortedTitles = titleCharacters.sort(function (a,b) {return b[1]-a[1]})
+// console.log(sortedTitles[0][0])
+
+const titleCharacters = characters.map(function(character) {return {'name':character.name, 'titles':character.titles.length}});
+const sortedTitles = titleCharacters.sort(function (a,b) {return b.titles-a.titles})
+console.log(sortedTitles[0].name)
+
+
 // How many are Valyrian?
+const culturedCharacters = characters.map(character => character.culture);
+
+const numValyrian = culturedCharacters.filter(culture => culture.toLowerCase() === 'valyrian').length;
+console.log(numValyrian)
+
 // What actor plays "Hot Pie" (and don't use IMDB)?
+// const actorCharacters = characters.map(character => [character.name, character.playedBy]);
+// const hotPieActor = actorCharacters.filter(characterLst => characterLst[0] === 'Hot Pie')[0][1];
+// console.log(hotPieActor[0]);
+
+const actorCharacters = characters.map(function (character) { return{'name':character.name, 'playedBy':character.playedBy}});
+const hotPieActor = actorCharacters.filter(characterLst => characterLst.name === 'Hot Pie')[0].playedBy;
+console.log(hotPieActor[0]);
+
 // How many characters are *not* in the tv show?
 // Produce a list characters with the last name "Targaryen"
 // Create a histogram of the houses (it's the "allegiances" key)
