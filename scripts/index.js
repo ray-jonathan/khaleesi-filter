bde5c8966f1e4a2e9a5b96348d9ced5f938b13b7
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-24 16:05:15 -0400

    Completed A name count, Z name count, Dead char count, most titles

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,33 @@
 console.log(`There are ${characters.length} characters in the array.`);
+console.log(characters[0]);
+console.log(characters[1]);
+console.log(characters[2]);
 
 // For each of these questions, write a function that returns the answer.
 
 // How many characters names start with "A"?
+const numChars = characters.map(character => character.name);
+
+function aFilter (name, letter) {
+    return name[0].toLowerCase() === letter.toLowerCase();
+}
+const aCharacters = numChars.filter(name=>name[0].toLowerCase() === 'a').length
+console.log(aCharacters);
+
 // How many characters names start with "Z"?
+const zCharacters = numChars.filter(name=>name[0].toLowerCase() === 'z').length
+console.log(zCharacters);
+
 // How many characters are dead?
+const deadCharacters = characters.map(character => character.died);
+const numDead = deadCharacters.filter(died => died).length
+console.log(numDead);
+
 // Who has the most titles?
+const titleCharacters = characters.map(character => [character.name, character.titles.length]);
+console.log(titleCharacters);
+const sortedTitles = titleCharacters.sort(function (a,b) {return b[1]-a[1]})
+console.log(sortedTitles[0][0])
 // How many are Valyrian?
 // What actor plays "Hot Pie" (and don't use IMDB)?
 // How many characters are *not* in the tv show?
